a7763444f93a13a62ff5e1505b7d80a4e907313b
seham <seham_hossni@outlook.com>
2023-10-05 16:20:12 -0700

    add new file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: C program that prints with puts Function
+ *
+ * Return: Always 0  (Success)
+*/
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
