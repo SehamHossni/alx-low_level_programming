0c575e222f3cc4bb314953db6288a486ec68fe8f
seham <seham_hossni@outlook.com>
2023-10-06 18:32:53 -0700

    some updates

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: print all alphabetic letters in lowercase.
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar('ch');
+		ch++;
+	}
+
+	ch = 'A';
+	while (ch <= 'Z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
